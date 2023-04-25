2006667313079634c89aac7172929c039a14062f
Ronen Gruengras <ronengr@gmail.com>
2023-04-25 16:49:40 +0300

    split nuki_manager out of nuki.py

--- nuki_bridge/nuki.py
+++ nuki_bridge/nuki.py
@@ -1,5 +1,4 @@
 import asyncio
-import datetime
 import hashlib
 import logging
 import hmac
@@ -8,7 +7,7 @@ import construct
 from asyncio import CancelledError, TimeoutError
 from typing import Callable
 from bleak.backends.device import BLEDevice
-from bleak.backends.scanner import AdvertisementData, AdvertisementDataCallback
+from bleak.backends.scanner import AdvertisementData
 from bleak.backends.characteristic import BleakGATTCharacteristic
 
 import async_timeout
@@ -19,94 +18,12 @@ from fastcrc import crc16
 import nacl.utils
 import nacl.secret
 from nacl.bindings.crypto_box import crypto_box_beforenm
-from bleak import BleakScanner, BleakClient, BleakError
+from bleak import BleakClient, BleakError
 from bleak.exc import BleakDBusError
 
 from .const import NukiErrorException, NukiLockConst, NukiOpenerConst, NukiConst
 
 logger = logging.getLogger(__name__)
-
-
-class NukiManager:
-    def __init__(self, name, app_id):
-        self.name = name
-        self.app_id = app_id
-        self.type_id = NukiConst.NukiClientType.BRIDGE
-        self._newstate_callback = None
-
-        self._devices = {}
-        self._scanner = BleakScanner(detection_callback=self._detected_ibeacon)
-
-    @property
-    def newstate_callback(self):
-        return self._newstate_callback
-
-    @newstate_callback.setter
-    def newstate_callback(self, value):
-        self._newstate_callback = value
-        for device in self._devices.values():
-            asyncio.get_event_loop().create_task(self.newstate_callback(device))
-
-    async def nuki_newstate(self, nuki):
-        if self.newstate_callback:
-            await self.newstate_callback(nuki)
-
-    def __getitem__(self, index):
-        return list(self._devices.values())[index]
-
-    def nuki_by_id(self, nuki_id):
-        #todo: will fail if no config yet.
-        return next(nuki for nuki in self._devices.values() if nuki.config.get("nuki_id") == nuki_id)
-
-    def add_nuki(self, nuki: 'Nuki'):
-        self._devices[nuki.address] = nuki
-
-    @property
-    def device_list(self):
-        return list(self._devices.values())
-
-    def start(self, loop=None):
-        pass
-
-    async def start_scanning(self):
-        ATTEMPTS = 8
-        logger.info(f"Starting a scan")
-        for i in range(1, ATTEMPTS + 1):
-            try:
-                logger.info(f"Scanning attempt {i}")
-                await self._scanner.start()
-                logger.info(f"Scanning succeeded on attempt {i}")
-                break
-            except BleakDBusError as ex:
-                logger.error(f"Error while start scanning attempt {i}")
-                logger.exception(ex)
-                if i >= ATTEMPTS - 1:
-                    raise ex
-                sleep_seconds = 2
-                logger.info(f"Scanning failed on attempt {i}. Retrying in {sleep_seconds} seconds")
-                time.sleep(sleep_seconds)
-
-    async def stop_scanning(self, timeout=10.0):
-        logger.info("Stop scanning")
-        try:
-            await asyncio.wait_for(self._scanner.stop(), timeout=timeout)
-            logger.info("Scanning stopped")
-        except (TimeoutError, CancelledError) as e:
-            logger.error(f'Timeout while stop scanning')
-            logger.exception(e)
-        except AttributeError as e:
-            logger.error('Error while stop scanning. Scan was probably not started.')
-            logger.exception(e)
-        except Exception as e:
-            logger.error('Error while stop scanning')
-            logger.exception(e)
-
-    async def _detected_ibeacon(self, device: BLEDevice, advertisement_data: AdvertisementData):
-        if device.address in self._devices:
-            nuki = self._devices[device.address]
-            await nuki.parse_advertisement_data(device, advertisement_data)
-            if nuki.poll_needed():
-                await nuki.update_state()
 
 class NukiDevice:
     def __init__(
